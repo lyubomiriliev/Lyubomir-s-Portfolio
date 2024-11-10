8f7d386f99cf6c8c2c7802d2a4220e8014931b3b
Lyubomir Iliev <iliev.lyubomir98@gmail.com>
2024-11-10 12:39:03 +0200

    last changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,69 +1,120 @@
-import React from 'react'
-import { githubSecondary, heroBlur, heroImageAnimate, heroImagePNG2, heroImagePNG3, heroTitle, linkedinSecondary, lyuboLogo } from '../assets/logos'
+import React from "react";
+import {
+  githubSecondary,
+  heroBlur,
+  heroImageAnimate,
+  heroImagePNG2,
+  heroImagePNG3,
+  heroTitle,
+  linkedinSecondary,
+  lyuboLogo,
+} from "../assets/logos";
 import { motion } from "framer-motion";
 
-
 const Hero = () => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/lyubomir-iliev-cv.pdf";
+    link.download = "lyubomir-iliev-cv.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
-    const handleDownload = () => {
-        const link = document.createElement('a');
-        link.href = '/lyubomir-iliev-cv.pdf'
-        link.download = "lyubomir-iliev-cv.pdf"
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
+  const scrolltoTop = () => {
+    scroll.scrollToTop({ smooth: true });
+  };
 
-    const scrolltoTop = () => {
-        scroll.scrollToTop({ smooth: true });
-    }
+  const scrollAnimation = {
+    animate: {
+      y: [0, -20, 0],
+    },
+    transition: {
+      duration: 4, // Total duration of the scroll (in seconds)
+      ease: "easeInOut", // Smooth easing for the movement
+      repeat: Infinity,
+      repeatType: "loop",
+    },
+  };
 
-    const scrollAnimation = {
-        animate:
-            {
-                y: [0, -20, 0],
-            },
-        transition: {
-            duration: 4, // Total duration of the scroll (in seconds)
-            ease: "easeInOut", // Smooth easing for the movement
-            repeat: Infinity,
-            repeatType: "loop",
-
-        },
-    };
-
-    return (
-        <>
-        <div className='max-w-screen-xl mx-auto flex flex-col md:mt-20 md:flex-row items-center relative'>
-            <div className='flex flex-col items-center md:items-start p-4 mt-16 md:-mt-20 md:p-0 z-20'>
-                <div onClick={scrolltoTop}>
-                            <img className='w-52 md:w-60 ml-2 mb-5' src={lyuboLogo} alt="Logo" />
-                </div>
-                <img className='w-full hidden md:flex' src={heroTitle} alt="Hero Title" />
-                <h3 className='font-bold md:hidden font-outfit text-5xl text-center text-secondary'>Front-End React Developer</h3>
-                <div className='hidden md:block'>
-                    <div className='w-1/4 md:w-1/3 ml-4 mt-2 h-[2px] bg-secondary'></div>
-                    <p className='w-3/4 md:w-2/3 ml-4 mt-8 text-primary font-outfit font-light text-lg'>Lorem ipsum dolor sit amet consectetur. Tellus quisque lacus semper tortor mauris porta.</p>
-                    <div className='flex items-center w-2/3 text-primary hover:text-secondary duration-300'>
-                        <span onClick={handleDownload} className='w-3/4 md:w-2/3 ml-4 mt-4 cursor-pointer font-medium font-outfit text-xl uppercase'>Download CV</span>
-                    </div>
-                    <div className='flex justify-start items-center mt-3 ml-4 gap-4'>
-                        <a href="https://www.linkedin.com/in/lyubomir-iliev-37ab32305/" target="_blank" rel="noopener noreferrer">
-                        <img className='w-10 md:w-14 hover:scale-110 duration-300' src={linkedinSecondary} alt="linkedin logo" />
-                        </a>
-                        <a href="https://github.com/lyubomiriliev" target="_blank" rel="noopener noreferrer">
-                        <img className='w-10 md:w-14 hover:scale-110 duration-300' src={githubSecondary} alt="github logo" />
-                        </a>
-
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="max-w-screen-xl 2xl:max-w-screen-2xl mx-auto flex flex-col md:mt-20 md:flex-row items-center relative">
+        <div className="flex flex-col items-center md:items-start p-4 mt-16 md:-mt-20 md:p-0 z-20">
+          <div onClick={scrolltoTop}>
+            <img
+              className="w-52 md:w-60 ml-2 mb-5"
+              src={lyuboLogo}
+              alt="Logo"
+            />
+          </div>
+          <img
+            className="w-full hidden md:flex"
+            src={heroTitle}
+            alt="Hero Title"
+          />
+          <h3 className="font-bold md:hidden font-outfit text-5xl text-center text-secondary">
+            Front-End React Developer
+          </h3>
+          <div className="hidden md:block">
+            <div className="w-1/4 md:w-1/3 ml-4 mt-2 h-[2px] bg-secondary"></div>
+            <p className="w-3/4 md:w-2/3 ml-4 mt-8 text-primary font-outfit font-light text-lg">
+              Lorem ipsum dolor sit amet consectetur. Tellus quisque lacus
+              semper tortor mauris porta.
+            </p>
+            <div className="flex items-center w-2/3 text-primary hover:text-secondary duration-300">
+              <span
+                onClick={handleDownload}
+                className="w-3/4 md:w-2/3 ml-4 mt-4 cursor-pointer font-medium font-outfit text-xl uppercase"
+              >
+                Download CV
+              </span>
             </div>
-            <img className='w-full -mt-4 md:w-2/3 z-20' src={heroImagePNG3} alt="Hero Image" />
-            <motion.img className='w-full -mt-4 md:w-2/3 right-0 bottom-0 z-20 absolute' src={heroImageAnimate} alt="Hero Image" {...scrollAnimation} />
+            <div className="flex justify-start items-center mt-3 ml-4 gap-4">
+              <a
+                href="https://www.linkedin.com/in/lyubomir-iliev-37ab32305/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="w-10 md:w-14 hover:scale-110 duration-300"
+                  src={linkedinSecondary}
+                  alt="linkedin logo"
+                />
+              </a>
+              <a
+                href="https://github.com/lyubomiriliev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="w-10 md:w-14 hover:scale-110 duration-300"
+                  src={githubSecondary}
+                  alt="github logo"
+                />
+              </a>
+            </div>
+          </div>
         </div>
-        <img src={heroBlur} alt="Hero Background" className='absolute top-0 left-0 -z-20 w-full h-full object-cover opacity-80' />
-        </>
-    )
-}
+        <img
+          className="w-full -mt-4 md:w-2/3 z-20"
+          src={heroImagePNG3}
+          alt="Hero Image"
+        />
+        <motion.img
+          className="w-full -mt-4 md:w-2/3 right-0 bottom-0 z-20 absolute"
+          src={heroImageAnimate}
+          alt="Hero Image"
+          {...scrollAnimation}
+        />
+      </div>
+      <img
+        src={heroBlur}
+        alt="Hero Background"
+        className="absolute top-0 left-0 -z-20 w-full h-full object-cover opacity-80"
+      />
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,61 +1,67 @@
-import { techStackLogos } from "../assets/logos"
+import { techStackLogos } from "../assets/logos";
 import { motion } from "framer-motion";
 
-
 const TechStack = () => {
+  const scrollAnimation = {
+    initial: { opacity: 0, scale: 0.8 },
+    animate: { opacity: 1, scale: 1 },
+    transition: {
+      duration: 2, // Total duration of the scroll (in seconds)
+      ease: "easeInOut", // Smooth easing for the movement
+    },
+  };
 
+  const filteredLogos = techStackLogos.filter(
+    (logo) => !["Redux2", "NextJs2", "Stripe"].includes(logo.name)
+  );
 
-    const scrollAnimation = {
-        initial: {opacity: 0, scale: 0.8},
-        animate: {opacity: 1, scale: 1,},
-        transition: {
-            duration: 2, // Total duration of the scroll (in seconds)
-            ease: "easeInOut", // Smooth easing for the movement
-        },
-    };
-
-    const filteredLogos = techStackLogos.filter(logo => !['Redux2', 'NextJs2', 'Stripe'].includes(logo.name))
-
-
-    return (
-        <>
-        {/* DESKTOP */}
-        <div className="relative hidden md:flex">
-            <div className="w-full py-4 md:h-48 flex items-center justify-center space-x-4 md:space-y-0 md:space-x-8 bg-gradient-to-r from-secondary to-purple-900">
-                <motion.div className="flex justify-center items-center mx-auto space-x-8" {...scrollAnimation} >
-                <h1 className="font-outfit font-bold text-sm md:text-4xl text-white">Tech Stack</h1>
-                <div className="w-[1px] md:w-[2px] bg-white h-6 md:h-12"></div>
-                {
-                    filteredLogos.map((logo, index) => (
-                        <motion.img 
-                        key={index} 
-                        className="w-5 md:w-16 rounded-md h-auto" 
-                        src={logo.src}
-                        alt="techStack logo"
-                        whileHover={{scale: 1.3}}
-                        />
-                    ))
-                }
-                </motion.div>
-            </div>
+  return (
+    <>
+      {/* DESKTOP */}
+      <div className="relative hidden md:flex">
+        <div className="w-full py-4 md:h-48 flex items-center justify-center space-x-4 md:space-y-0 md:space-x-8 bg-gradient-to-r from-secondary to-purple-900">
+          <motion.div
+            className="flex justify-center items-center mx-auto space-x-8"
+            {...scrollAnimation}
+          >
+            <h1 className="font-outfit font-bold text-sm md:text-4xl text-white">
+              Tech Stack
+            </h1>
+            <div className="w-[1px] md:w-[2px] bg-white h-6 md:h-12"></div>
+            {filteredLogos.map((logo, index) => (
+              <motion.img
+                key={index}
+                className="w-5 md:w-16 rounded-md h-auto"
+                src={logo.src}
+                alt="techStack logo"
+                whileHover={{ scale: 1.3 }}
+              />
+            ))}
+          </motion.div>
         </div>
+      </div>
 
-        {/* MOBILE */}
-        <div className="w-full h-60 flex flex-col bg-gradient-to-r from-secondary to-purple-900 justify-center items-center md:hidden">
-            <div className="flex">
-                <h1 className="font-outfit font-bold text-3xl text-white">Tech Stack</h1>
-            </div>
-            <div className="w-[90%] grid grid-cols-6 mt-5 justify-center gap-4 h-40 items-center">
-                {
-                    filteredLogos.map((logo, index) => (
-                            <motion.img key={index} className="w-16 rounded-sm" src={logo.src} alt="techStack logo" {...scrollAnimation}  />
-                    ))
-                }
-            </div>
+      {/* MOBILE */}
+      <div className="w-full h-60 flex flex-col bg-gradient-to-r from-secondary to-purple-900 justify-center items-center md:hidden">
+        <div className="flex">
+          <h1 className="font-outfit font-bold text-3xl text-white">
+            Tech Stack
+          </h1>
         </div>
-        </>
+        <div className="w-[90%] grid grid-cols-6 mt-5 justify-center gap-4 h-40 items-center">
+          {filteredLogos.map((logo, index) => (
+            <motion.img
+              key={index}
+              className="w-16 rounded-sm"
+              src={logo.src}
+              alt="techStack logo"
+              {...scrollAnimation}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-    )
-}
-
-export default TechStack
+export default TechStack;
