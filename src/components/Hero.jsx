6a1c26272a61881a81af128cf5bd01b46460a412
Lyubomir Iliev <iliev.lyubomir98@gmail.com>
2024-11-10 12:46:21 +0200

    last hero change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,6 @@ import {
 } from "../assets/logos";
 import { motion } from "framer-motion";
 
-
 const Hero = () => {
   const handleDownload = () => {
     const link = document.createElement("a");
@@ -38,31 +37,62 @@ const Hero = () => {
     },
   };
 
-    return (
-        <>
-        <div className='max-w-screen-xl mx-auto flex flex-col md:mt-20 md:flex-row items-center relative'>
-            <div className='flex flex-col items-center md:items-start p-4 mt-16 md:-mt-20 md:p-0 z-20'>
-                <div onClick={scrolltoTop}>
-                            <img className='w-52 md:w-60 ml-2 mb-5' src={lyuboLogo} alt="Logo" />
-                </div>
-                <img className='w-full hidden md:flex' src={heroTitle} alt="Hero Title" />
-                <h3 className='font-bold md:hidden font-outfit text-5xl text-center text-secondary'>Front-End React Developer</h3>
-                <div className='hidden md:block'>
-                    <div className='w-1/4 md:w-1/3 ml-4 mt-2 h-[2px] bg-secondary'></div>
-                    <p className='w-3/4 md:w-2/3 ml-4 mt-8 text-primary font-outfit font-light text-lg'>Lorem ipsum dolor sit amet consectetur. Tellus quisque lacus semper tortor mauris porta.</p>
-                    <div className='flex items-center w-2/3 text-primary hover:text-secondary duration-300'>
-                        <span onClick={handleDownload} className='w-3/4 md:w-2/3 ml-4 mt-4 cursor-pointer font-medium font-outfit text-xl uppercase'>Download CV</span>
-                    </div>
-                    <div className='flex justify-start items-center mt-3 ml-4 gap-4'>
-                        <a href="https://www.linkedin.com/in/lyubomir-iliev-37ab32305/" target="_blank" rel="noopener noreferrer">
-                        <img className='w-10 md:w-14 hover:scale-110 duration-300' src={linkedinSecondary} alt="linkedin logo" />
-                        </a>
-                        <a href="https://github.com/lyubomiriliev" target="_blank" rel="noopener noreferrer">
-                        <img className='w-10 md:w-14 hover:scale-110 duration-300' src={githubSecondary} alt="github logo" />
-                        </a>
-
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="max-w-screen-xl 2xl:max-w-screen-2xl mx-auto flex flex-col md:mt-20 md:flex-row items-center relative">
+        <div className="flex flex-col items-center md:items-start p-4 mt-16 md:-mt-20 md:p-0 z-20">
+          <div onClick={scrolltoTop}>
+            <img
+              className="w-52 md:w-60 ml-2 mb-5"
+              src={lyuboLogo}
+              alt="Logo"
+            />
+          </div>
+          <img
+            className="w-full hidden md:flex"
+            src={heroTitle}
+            alt="Hero Title"
+          />
+          <h3 className="font-bold md:hidden font-outfit text-5xl text-center text-secondary">
+            Front-End React Developer
+          </h3>
+          <div className="hidden md:block">
+            <div className="w-1/4 md:w-1/3 ml-4 mt-2 h-[2px] bg-secondary"></div>
+            <p className="w-3/4 md:w-2/3 ml-4 mt-8 text-primary font-outfit font-light text-lg">
+              Lorem ipsum dolor sit amet consectetur. Tellus quisque lacus
+              semper tortor mauris porta.
+            </p>
+            <div className="flex items-center w-2/3 text-primary hover:text-secondary duration-300">
+              <span
+                onClick={handleDownload}
+                className="w-3/4 md:w-2/3 ml-4 mt-4 cursor-pointer font-medium font-outfit text-xl uppercase"
+              >
+                Download CV
+              </span>
+            </div>
+            <div className="flex justify-start items-center mt-3 ml-4 gap-4">
+              <a
+                href="https://www.linkedin.com/in/lyubomir-iliev-37ab32305/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="w-10 md:w-14 hover:scale-110 duration-300"
+                  src={linkedinSecondary}
+                  alt="linkedin logo"
+                />
+              </a>
+              <a
+                href="https://github.com/lyubomiriliev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="w-10 md:w-14 hover:scale-110 duration-300"
+                  src={githubSecondary}
+                  alt="github logo"
+                />
+              </a>
             </div>
           </div>
         </div>
