55adebf3863c11eaa6a936bb9daa6327975f1bf9
Lyubomir Iliev <iliev.lyubomir98@gmail.com>
2024-11-21 16:29:08 +0200

    small fix for mobile

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -26,13 +26,17 @@ const Project = ({
     <div className="w-full flex h-[450px] md:h-[540px] space-x-6 px-4 py-4 rounded-[24px] group relative">
       <div className="w-full flex flex-col justify-around rounded-[24px] bg-gradient-to-r from-slate-200 via-slate-50 to-slate-200 overflow-hidden relative select-none">
         <div className="flex justify-center items-center p-2">
-          <img src={imgSrc} alt={title} className="w-[75%] mt-6 select-none" />
+          <img
+            src={imgSrc}
+            alt={title}
+            className="w-[85%] lg:w-[75%] mt-6 select-none"
+          />
         </div>
         <div>
           <img
             src={mobileImg}
             alt={title}
-            className="w-36 select-none absolute top-28 right-2"
+            className="w-[108px] lg:w-36 select-none absolute top-28 right-2"
           />
         </div>
         <a href={webLink} target="_blank" rel="noopener noreferrer">
